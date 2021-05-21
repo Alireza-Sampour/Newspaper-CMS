41e90c144a66d813ba450857d57f12cbfd9402ce
Alireza-Sampour <AlirezaSampoor@gmail.com>
2021-05-21 20:31:58 +0430

    remove unnecessary import

--- pages/forms.py
+++ pages/forms.py
@@ -2,7 +2,6 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 from django import forms
-from django.utils.translation import gettext_lazy as _
 
 
 class CustomUserCreationForm(UserCreationForm):
